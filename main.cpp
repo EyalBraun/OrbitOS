8604939b8df3200b00150d0c00ec0f6b0960ed86
Eyal Braun <cseyal881@gmail.com>
2026-03-01 08:54:32 +0200

    Commit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <vector>
 using namespace std;
-//all of the commands functions in an array.
-string commands_functions[64];
-// show the base UserName and curr Dir.
-
-void base_display(string name, string dir){
-  cout<<name<<":"<<dir<<endl;
-}
+//
+string commands[64];
 
 void compile_command(string c){
   // the index is = ((f*31) + l ) %64
@@ -16,11 +12,32 @@ void compile_command(string c){
   
   
 }
-int main(){
+void base_display(string name,string dir){
+  cout<<name<<":"<<dir<<endl;
+}
+int get_idx(string s){
+  return ((s[0]*31) + s[s.size()-1]) %64;
   
+}
+void build_commands(){
+  fstream file("commands.txt");
+string line;
+while (getline(file, line)) {
+       int idx = get_idx(line);
+        commands[idx] = line;
+
+     
+    }
+
+}
+int main(){
+  build_commands();
   
   string name; cout<<"Whats the Owner Name ? : "; cin >> name;
   string dir = "root";
+  for(int i = 0;i<64;i++){
+    if(commands[i] != "")cout<<commands[i] << endl;
+  }
   while(true){
         base_display(name,dir);
 
@@ -34,6 +51,9 @@ int main(){
     
   }
   
+
+
+   
 
 
 
